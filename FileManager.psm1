aa91d873c57f28dfc738500d0e2b7753522f3b13
Igor Miloradovic <imiloradovic@globalcommunities.org>
2023-06-13 12:05:30 +0200

    Updated Pipeline.ps1

--- FileManager.psm1
+++ FileManager.psm1
@@ -6,12 +6,19 @@ class FileManager {
         else { New-Item -ItemType Directory -Path $folderPath -ErrorAction Stop }
     }
 
+    static [void] DeleteFolder([string] $folderPath)
+    {
+        Remove-Item -Path $folderPath -Force -Recurse -ErrorAction SilentlyContinue
+    }
+
     static [void] CreateTestFile ([string] $filePath, [int] $fileSizeKb)
     {
-        $byteArray = New-Object -TypeName Byte[] -ArgumentList $fileSizeKb
-        $randomGenerator = New-Object -TypeName System.Random
+        [Byte[]] $byteArray = [Byte[]]::new($fileSizeKb * 512)
+        $randomGenerator = [System.Random]::new()
         $randomGenerator.NextBytes($byteArray)
-        Set-Content -Path $filePath -Value $byteArray.ToString() 
+
+        [string] $fileContent = [System.Text.Encoding]::UTF8.GetString($byteArray)
+        Set-Content -Path $filePath -Value $fileContent
     }
 }
 
--- Pipeline.ps1
+++ Pipeline.ps1
@@ -1,17 +1,26 @@
 using module ./FileManager.psm1
 
-# function Pipeline01
-# {
-    # Import-Module ./FileManager.psm1
-    # in this example, we create 4 text files with different sizes and delete all of them, except two largest
-    $guid = New-Guid
-    $testFolderPath = "$PSScriptRoot\$guid"
+function Pipeline01
+{
+    # in this example, we create 4 text files with different sizes and delete two largest with extension .log
+
+    $testFolderPath = "$PSScriptRoot\$(New-Guid)"
     [FileManager]::CreateFolder($testFolderPath)
 
-    [FileManager]::CreateTestFile("$testFolderPath\file01.txt", 1)
-    [FileManager]::CreateTestFile("$testFolderPath\file02.txt", 2)
-    [FileManager]::CreateTestFile("$testFolderPath\file03.txt", 3)
-    [FileManager]::CreateTestFile("$testFolderPath\file04.txt", 4)
-# }
+    # create files with specified sizes
+    [FileManager]::CreateTestFile("$testFolderPath\file01.log", 1)
+    [FileManager]::CreateTestFile("$testFolderPath\file02.log", 2)
+    [FileManager]::CreateTestFile("$testFolderPath\file03.log", 3)
+    [FileManager]::CreateTestFile("$testFolderPath\file04.log", 4)
 
-# & Pipeline01
+    [FileManager]::CreateTestFile("$testFolderPath\file05.txt", 5)
+    [FileManager]::CreateTestFile("$testFolderPath\file06.txt", 6)
+
+    # delete two largets files of type .log
+    Get-ChildItem -Path $testFolderPath | Where-Object { $_.Extension -eq '.log' } | Sort-Object { $_.Length } -Descending `
+        | Select-Object -First 2 | Remove-Item -Force
+    # cleanup
+    [FileManager]::DeleteFolder($testFolderPath)
+}
+
+& Pipeline01
